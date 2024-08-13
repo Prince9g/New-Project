1793bcf562123ce381ee7e70d17bc7e2205263aa
Prince9g <142648202+Prince9g@users.noreply.github.com>
2024-08-13 23:26:28 +0530

    done it with useRef(), by terminate it with useState() HOOk

--- to-do-list/src/components/AddTodo.jsx
+++ to-do-list/src/components/AddTodo.jsx
@@ -1,36 +1,54 @@
-import {useState} from 'react';
-function AddTodo({onNewItem}){
-  //by using states
-    const [todoName, setTodoName] = useState();
-    const [dueDate, setDueDate] = useState();
-//made a simple function to handle the name change that need to add
-    const handleNameChange = (event) => {
-      setTodoName(event.target.value);
-    }
-    //made another function that handles date change from user input
-    const handleDateChange = (event) => {
-      setDueDate(event.target.value);
-    }
-    //handling add button 
-    const handleAddButton = () =>{
-      onNewItem(todoName, dueDate);
-      setDueDate("");
-      setTodoName("");
-    }
-    return (
-      <div className="row my-row">
+import { useRef } from "react";
+function AddTodo({ onNewItem }) {
+  // //by using states
+  // const [todoName, setTodoName] = useState();
+  // const [dueDate, setDueDate] = useState();
+  // //made a simple function to handle the name change that need to add
+  // const handleNameChange = (event) => {
+  //   setTodoName(event.target.value);
+  // };
+  // //made another function that handles date change from user input
+  // const handleDateChange = (event) => {
+  //   setDueDate(event.target.value);
+  // };
+
+  //doing it by useRef
+  const todoitemName = useRef();
+  const todoitemDate = useRef();
+  //handling add button
+  const handleAddButton = () => {
+    const todoName = todoitemName.current.value;
+    const dueDate = todoitemDate.current.value;
+    onNewItem(todoName, dueDate);
+    todoitemName.current.value='';
+    todoitemDate.current.value='';
+  };
+  return (
+    <div className="row my-row">
       <div className="col-6">
-      <input type="text" placeholder="Enter to-do Here" 
-      value={todoName} onChange={handleNameChange}></input>
+        <input
+          type="text"
+          ref={todoitemName}
+          placeholder="Enter to-do Here"
+          // value={todoName}
+          // onChange={handleNameChange}
+        ></input>
       </div>
       <div className="col-4">
-      <input type="date" value={dueDate} onChange={handleDateChange}></input>
+        <input type="date" ref={todoitemDate}></input>
+        {/* // value={dueDate} onChange={handleDateChange} */}
       </div>
       <div className="col-2">
-      <button type="button" className="btn btn-success my-button" onClick={handleAddButton}>Add</button>
+        <button
+          type="button"
+          className="btn btn-success my-button"
+          onClick={handleAddButton}
+        >
+          Add
+        </button>
       </div>
     </div>
-    )
+  );
 }
 
 export default AddTodo;
